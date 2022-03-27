75ca376523bf334add2352d67c9ca9a169e9c8a2
alfred3ks <alfred3ks@users.noreply.github.com>
2022-03-27 09:22:34 +0000

    deploy: d7d7e0cf12151b4dad7f4dee5d16ef90e9bd3828

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,38 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// Conexion con la API:
+const url = 'https://platzi-avo.vercel.app/api/avo';
 
-console.log('Happy hacking :)')
+// Web api usando fetch
+// conectarnos al servidor usando promesas
+window
+    .fetch(url)
+    // procesar la respuesta y convertirla en JSON
+    .then(respuesta => respuesta.json())
+    // JSON -> Data -> renderizar info en browser
+    .then((responseJson) => {
+
+        // Creamos un array para meter los container
+        const todosLosItems = [];
+
+        // console.log(responseJson);
+        // Para obtener los elementos hacemos un ciclo
+        responseJson.data.forEach(item => {
+            // console.log(item);
+            // console.log(item.name);
+            // Creamos los items:
+            const image = document.createElement('img');
+            const title = document.createElement('h2');
+            const price = document.createElement('div');
+            
+
+            // Creamos un contenedor para image,title,price
+            const container = document.createElement('div');
+            container.append(image, title, price);
+
+            // Metemos todo en el array
+            todosLosItems.push(container);
+
+        });
+        // Agregamos el container al body
+        document.body.append(...todosLosItems);
+    });
+
